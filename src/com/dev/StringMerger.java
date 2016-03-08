c3ff44cd3a03e0ea3d2f63366f929b37405e1a5c
John Orr <john.orr@bearingpoint.com>
2016-03-08 16:27:11 +0000

    StringMerger

--- src/com/dev/StringMerger.java
+++ src/com/dev/StringMerger.java
@@ -10,11 +10,7 @@ public class StringMerger {
             return part1.isEmpty() && part2.isEmpty();
         }
         char c = s.charAt(0);
-        if (!part1.isEmpty() && c == part1.charAt(0))
-            if (isMerge(s.substring(1), part1.substring(1), part2))
-                return true;
-        if (!part2.isEmpty() && c == part2.charAt(0))
-            return isMerge(s.substring(1), part1, part2.substring(1));
-        return false;
+        return !part1.isEmpty() && c == part1.charAt(0) && isMerge(s.substring(1), part1.substring(1), part2) ||
+                !part2.isEmpty() && c == part2.charAt(0) && isMerge(s.substring(1), part1, part2.substring(1));
     }
 }
